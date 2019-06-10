80babffac41b30784d4f9adf7415208453abf455
chen <1123255497@qq.com>
2019-06-10 23:27:56 +0800

    mybatis初始化demo

---
+++ src/main/java/com/company/project/service/UserService.java
@@ -0,0 +1,12 @@
+package com.company.project.service;
+
+import com.company.project.core.Service;
+import com.company.project.model.User;
+
+
+/**
+ * Created by CodeGenerator on 2019/06/10.
+ */
+public interface UserService extends Service<User> {
+
+}
